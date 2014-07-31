57116a721e6f5c80b1cac1e6c7d15b0aed23d923
Colin Bookman <cobookman@gmail.com>
2014-07-30 21:12:36 -0700

    heroku doesn't like me :(

--- app.js
+++ app.js
@@ -43,8 +43,8 @@ app.get('/', routes.index);
 //Get Partials
 hbs.registerPartials(__dirname + '/views/partials');
 
-
-http.createServer(app).listen(80, function(){
+var port = Number(process.env.PORT || 5000);
+http.createServer(app).listen(port, function(){
  console.log('Express server listening on port 80');
 });
 
