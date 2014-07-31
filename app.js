e3cb507d3a08471c7cdac729290055062819e1d4
Colin Bookman <cobookman@gmail.com>
2014-07-30 21:10:40 -0700

    Forgot to change ports

--- app.js
+++ app.js
@@ -44,8 +44,8 @@ app.get('/', routes.index);
 hbs.registerPartials(__dirname + '/views/partials');
 
 
-http.createServer(app).listen(8080, function(){
- console.log('Express server listening on port 8080');
+http.createServer(app).listen(80, function(){
+ console.log('Express server listening on port 80');
 });
 
 
